e92ab6785dcb4487038a5bcc9073c12608308e31
Gabe Bravo <gabecbravo@gmail.com>
2021-01-24 21:40:03 -0500

    basic working version of recoil and ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useRecoilState} from 'recoil';
+import { counterState } from './atoms/atoms'
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useRecoilState(counterState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Recoil Typescript</h2>
+      <h4>{counter}</h4>
+      <button onClick={() => setCounter(counter + 1)}>Inc</button>
     </div>
   );
 }
